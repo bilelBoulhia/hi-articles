af5571e674c645da30c5a7ba9c3b377d8e204b1b
bilel0509 <billel.boulahia456@gmail.com>
2024-11-03 00:15:41 +0100

    fixing navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,54 @@
-import React from 'react';
-import './Header.css';
-import {FaSearch} from 'react-icons/fa'
-import Logo from '../../images/Logo.svg'
-import { Dropdown, DropdownButton } from "react-bootstrap";
-
+import React from "react";
+import "./Header.css";
+import { FaSearch } from "react-icons/fa";
+import Logo from "../../images/Logo.svg";
 
 const Header = () => {
   return (
-    <div className="container">
-      
-     <div className="nav-right">
-       <div className="img">
-         <img className='logo' src={Logo} />
-       </div>
-      
-      <div className="links">
-        <li className='website'><a href="https://houseofideas.club" target="_blank">Main Website</a></li>
-     </div>
+    <nav className="nav-container">
+      <div className="nav-left">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            className="logo"
+            src="https://i.ibb.co/YLhbmxZ/hiicon.png"
+            alt="img"
+          />
+        </div>
+        <div>
+          <a href="https://houseofideas.club">Main Website</a>
+        </div>
+        <div className="dropdown">
+          <input type="checkbox" id="dropdown" />
 
-     <DropdownButton
-      id="dropdown-custom-button"
-      title="Other Topics"
-      className="custom-btn"
-      variant="secondary" 
-    >
-      <Dropdown.Item href="#">Action</Dropdown.Item>
-      <Dropdown.Item href="#">Another action</Dropdown.Item>
-      <Dropdown.Item href="#">Something else here</Dropdown.Item>
-    </DropdownButton>
-      </div>
-      
-      <div className="search">
-       <FaSearch id='search-icon'/>
-       <input placeholder='search...' type="text" />
+          <label className="dropdown__face" htmlFor="dropdown">
+            <div className="dropdown__text">Topics</div>
+
+            <div className="dropdown__arrow"></div>
+          </label>
+
+          <ul className="dropdown__items">
+            <li>Drama</li>
+            <li>Economics</li>
+            <li>Trading</li>
+            <li>Death</li>
+            <li>imperium</li>
+          </ul>
+        </div>
       </div>
 
-      
-   
-    </div>
+      <div className="nav-right">
+        <div className="search">
+          <FaSearch id="search-icon" />
+          <input placeholder="search..." type="text" />
+        </div>
+      </div>
+    </nav>
   );
 };
 
