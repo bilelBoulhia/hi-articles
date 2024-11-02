c7524a078c9a3c7e2e0a3a0bf2ddda44193877b3
bilel0509 <billel.boulahia456@gmail.com>
2024-11-02 20:56:33 +0100

    adding slider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero'
-import Cards from './components/cards/cards'
-
+import Slider from "./components/cards/slider";
+import './components/cards/embla.css'
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <Cards />
+      <Slider  slides={Array.from(Array(5).keys())} options={{ align: 'start', dragFree: true, loop: true }} />
     </>
   );
 }
