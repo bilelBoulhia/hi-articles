345a296e282038a2b4f24770385a7bdb2242d775
hsenaouragh <hsenaouragh@gmail.com>
2024-11-22 19:33:18 +0100

    ejournal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero'
+/*import Hero from './components/Hero/Hero'
 import Slider from "./components/cards/slider";
-import './components/cards/embla.css';
+import './components/cards/embla.css';*/
 
-/*import Landing from "./components/landing-page/Landing";*/
+import Landing from "./components/landing-page/Landing";
 import Footer from './components/Footer/Footer';
 
 
 function App() {
   return (
-   /* <>
+   /*<>
+   <Hero /> 
+   <Slider  slides={Array.from(Array(5).keys())} options={{ align: 'start', dragFree: true, loop: true }} />
+   </>*/
+   <>
+    <Header />
     <Landing />
-    </>*/
-    <>
-      <Header />
-      <Hero />
-      <Slider  slides={Array.from(Array(5).keys())} options={{ align: 'start', dragFree: true, loop: true }} />
-      <Footer />
+    <Footer />
     </>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Footer.css'
+import tiktok from '../../images/icons8-tiktok-50.png'
+import instagram from '../../images/icons8-instagram-50.png'
+import linkedin from '../../images/icons8-linkedin-50.png'
+import facebook from '../../images/icons8-facebook-50.png'
 
 const Footer = () => {
   return (
     <div className="footer">
-      
+      <div className="infos">
+        <ul className='contacts'><h2>Contact us:</h2>
+          <li>Phone Number: <a href="">0555442266</a></li>
+          <li>Email: <a href="">houseofideas@gmail.com</a></li>
+          <li>location: <a href="">université d'Alger 3, Dely Brahim</a></li>
+        </ul>
+        <ul className='medias'><h2>Our Medias:</h2>
+          <li><img src={tiktok} href=""/></li>
+          <li><img src={instagram} href=""/></li>
+          <li><img src={linkedin} alt="" /></li>
+          <li><img src={facebook} alt="" /></li>
+        </ul>
+      </div>
     </div>
   )
 }
