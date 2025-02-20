b450962fbff58748fcf5644a7a70ccea5e5f1c5e
bilelBoulhia <billel.boulahia456@gmail.com>
2025-02-20 06:05:17 +0100

    fixing header

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,8 @@ export default async function Home() {
   return (
 
       <div className="flex justify-center items-center h-screen w-full relative flex-row px-4">
-
           <div className=' text-xl flex-col items-start justify-center    relative  w-[60%]'>
-              <span className='text-4xl md:text-7xl tracking-[0.60rem] font-bold  bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500'>Welcome to </span>
-              <WordTransform className='text-4xl md:text-7xl font-bold ' initialWord="SECRETARIAT" transformedWord="HI-EJOURNAL"/>
+              <WordTransform className="font-bold text-8xl" initialWord="WELCOME TO  " transformedWord="HI E-JOURNAL" />
           </div>
           <div className=' flex flex-col justify-center items-center h-full  w-[40%]'>
 
--- components/ui/animated-headingV2.tsx
+++ components/ui/animated-headingV2.tsx
@@ -1,69 +1,89 @@
-"use client"
+"use client";
 
-import type React from "react"
-
-import { motion, AnimatePresence } from "framer-motion"
-
+import type React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { cn } from "@/lib/utils";
 
 interface LetterPairProps {
-    initialLetter: string
-    transformedLetter: string
-
-    delay: number
+    initialLetter: string;
+    transformedLetter: string;
+    index: number;
+    delay: number;
 }
 
-const LetterPair: React.FC<LetterPairProps> = ({ initialLetter, transformedLetter, delay }) => {
+const LetterPair: React.FC<LetterPairProps> = ({
+                                                   index,
+                                                   initialLetter,
+                                                   transformedLetter,
+                                                   delay,
+                                               }) => {
     return (
-        <div className="relative inline-block w-[3.25rem] text-center h-[1.2em] ">
+        <motion.span
+            style={{ transform: `translateX(${index}ch)` }}
+            className="inline-block mt-1 relative will-change-transform"
+        >
             <AnimatePresence>
                 <motion.span
                     key={`i${initialLetter}-${delay}`}
                     initial={{ height: "auto" }}
                     animate={{ height: 0 }}
                     exit={{ height: 0 }}
-                    transition={{ duration: 0.5, delay: delay / 1000 }}
-                    className="absolute overflow-hidden  inset-x-0 top-0"
+                    transition={{ duration: 0.5, delay: delay / 1500 }}
+                    className="absolute overflow-hidden left-0 top-0"
                 >
                     {initialLetter}
                 </motion.span>
+
                 <motion.span
                     key={`t${transformedLetter}-${delay}`}
                     initial={{ height: 0 }}
                     animate={{ height: "auto" }}
                     exit={{ height: 0 }}
-                    transition={{ duration: 0.5, delay: delay / 1000 }}
-                    className="absolute overflow-hidden  inset-x-0 bottom-0"
+                    transition={{ duration: 0.5, delay: delay / 1500 }}
+                    className="absolute overflow-hidden left-0 bottom-0"
                 >
                     {transformedLetter}
                 </motion.span>
             </AnimatePresence>
-        </div>
-    )
-}
+        </motion.span>
+    );
+};
 
 interface WordTransformProps {
-    initialWord: string
-    transformedWord: string
-    className?: string
-
+    initialWord: string;
+    transformedWord: string;
+    className?: string;
 }
 
-const WordTransform: React.FC<WordTransformProps> = ({ className, initialWord, transformedWord }) => {
-    const letterPairs: Array<[string, string]> = initialWord.split("").map((letter, index) => [letter, transformedWord[index]]);
+const WordTransform: React.FC<WordTransformProps> = ({
+                                                         className,
+                                                         initialWord,
+                                                         transformedWord,
+                                                     }) => {
+    const letterPairs: Array<[string, string]> = initialWord
+        .split("")
+        .map((letter, index) => [letter, transformedWord[index]]);
+
     return (
-        <div className={className}>
+        <div
+
+            style={{
+                fontFamily:'"Liberation Mono"'
+            }}
+
+            className={cn(className, "whitespace-nowrap")}>
             {letterPairs.map((pair, index) => (
                 <LetterPair
                     key={index}
                     initialLetter={pair[0]}
                     transformedLetter={pair[1]}
                     delay={index * 100}
+                    index={index}
                 />
             ))}
         </div>
     );
 };
 
-
-export default WordTransform
+export default WordTransform;
 
