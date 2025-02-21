ef12af6e305a308705424c4cac3a2e45734a340e
bilelBoulhia <billel.boulahia456@gmail.com>
2025-02-21 08:33:03 +0100

    finishing the hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <ThemeProvider
           attribute="class"
 
-          defaultTheme='ligth'
+          defaultTheme='dark'
           enableSystem
           disableTransitionOnChange
       >
