22ac38a5ee37bb142c3adf01bb121373c168ef78
bilelBoulhia <billel.boulahia456@gmail.com>
2025-02-22 02:57:15 +0100

    finishing the hero

--- app/(main page)/sections/hero.tsx
+++ app/(main page)/sections/hero.tsx
@@ -1,12 +1,10 @@
-'use client'
+import img2 from "@/app/assets/img2.png";
+import img1 from "@/app/assets/img1.jpg";
+import img3 from "@/app/assets/img3.png";
+import img4 from "@/app/assets/img4.png";
 import WordTransform from "@/components/ui/animated-headingV2";
 import { motion } from "motion/react";
 import Carousel from "@/components/ui/Carousel";
-import img1 from "@/app/assets/img1.jpg"
-import img2 from "@/app/assets/img2.png"
-import img3 from "@/app/assets/img3.png"
-import img4 from "@/app/assets/img4.png"
-
 
 const paragraph= 'House of Ideas E-Journal, a website run by Secretariat Department, sharing articles on science, e-commerce & finance, and many many more, adding a voice to the club and its community.'
 
@@ -54,80 +52,74 @@ const articles = [
     could the DEEP SEEK R1 be a game-changer for the future of automation?`
     }
 ];
+export  default function Hero(){
+    return (
+        <div
+            className="flex justify-center items-center lg:flex-row  w-full relative flex-col px-4">
 
+            <div
+                className='relative flex-col gap-[1rem] text-5xl [@media(min-width:450px)]:text-7xl sm:text-8xl min-h-screen xl:min-h-full  justify-center items-start flex w-full xl:w-[60%]'>
 
-export default  function Home() {
-  return (
-      <>
-          <div
-              className="flex justify-center items-center lg:flex-row  w-full relative flex-col px-4">
+                <motion.span
+                    initial={{x: "-150%"}}
+                    animate={{x: "0%"}}
+                    transition={{duration: 1, delay: 1.5}}
+                    className='bg-clip-text font-bold text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500'>
+                    HOUSE OF IDEAS
+                </motion.span>
 
-              <div
-                  className='relative flex-col gap-[1rem] text-5xl [@media(min-width:450px)]:text-7xl sm:text-8xl min-h-screen xl:min-h-full  justify-center items-start flex w-full xl:w-[60%]'>
-
-                  <motion.span
-                      initial={{x: "-150%"}}
-                      animate={{x: "0%"}}
-                      transition={{duration: 1, delay: 1.5}}
-                      className='bg-clip-text font-bold text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500'>
-                      HOUSE OF IDEAS
-                  </motion.span>
-
-                  <span className='text-center'>
+                <span className='text-center'>
                   <WordTransform
                       className="font-bold "
                       initialWord="WELCOME TO "
                       transformedWord="E-JOURNAL"/>
               </span>
 
-                  <motion.div
-                      initial={{opacity: 0}}
-                      animate={{opacity: 1}}
-                      transition={{duration: 1, delay: 2}}
-                      style={{
-                          fontFamily: '"Liberation Mono"',
-                          perspective: "1000px",
-                      }}
-                      className="text-xl font-medium font-mono text-left "
-                  >
-                      {paragraph.split(",").map((sentence, index) => (
-                          <motion.span
+                <motion.div
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    transition={{duration: 1, delay: 2}}
+                    style={{
+                        fontFamily: '"Liberation Mono"',
+                        perspective: "1000px",
+                    }}
+                    className="text-xl font-medium font-mono text-left "
+                >
+                    {paragraph.split(",").map((sentence, index) => (
+                        <motion.span
 
-                              className="inline-block mr-2"
-                              key={index}
-                              initial={{
-                                  opacity: 0,
-                                  y: 40,
-                                  scale: 0.98,
-                              }}
-                              animate={{
-                                  opacity: 1,
-                                  y: 0,
-                                  scale: 1,
-                              }}
-                              transition={{
-                                  duration: 0.8,
-                                  delay: index * 0.5 *0.8,
-                                  ease: [0.2, 0.65, 0.3, 0.9],
-                              }}
-                          >
-                              {index === paragraph.split(",").length - 1 ? sentence.trim() : sentence.trim() + ","}
-                          </motion.span>
-                      ))}
-                  </motion.div>
-              </div>
+                            className="inline-block mr-2"
+                            key={index}
+                            initial={{
+                                opacity: 0,
+                                y: 40,
+                                scale: 0.98,
+                            }}
+                            animate={{
+                                opacity: 1,
+                                y: 0,
+                                scale: 1,
+                            }}
+                            transition={{
+                                duration: 0.8,
+                                delay: index * 0.5 * 0.8,
+                                ease: [0.2, 0.65, 0.3, 0.9],
+                            }}
+                        >
+                            {index === paragraph.split(",").length - 1 ? sentence.trim() : sentence.trim() + ","}
+                        </motion.span>
+                    ))}
+                </motion.div>
+            </div>
 
-              <motion.div
-                  initial={{x: "100%"}}
-                  animate={{x: "0"}}
-                  transition={{duration: 1, delay: 1.5}}
-                  className=' flex flex-col justify-center items-center h-full      w-full   xl:w-[40%]'>
-                  <Carousel articles={articles}/>
-              </motion.div>
+            <motion.div
+                initial={{x: "100%"}}
+                animate={{x: "0"}}
+                transition={{duration: 1, delay: 1.5}}
+                className=' flex flex-col justify-center items-center h-full      w-full   xl:w-[40%]'>
+                <Carousel articles={articles}/>
+            </motion.div>
 
-          </div>
-
-
-      </>
-  );
+        </div>
+    )
 }
--- app/sections/Header.tsx
+++ app/sections/Header.tsx
@@ -5,10 +5,10 @@ import React, {useState} from "react";
 import SearchModal from "@/components/search-modal";
 
 export default function Header(){
-    const [searchQuery, setSearchQuery] = useState("");
-    const handleSearchQuery = function (searchQuery: string) {
-        setSearchQuery(searchQuery);
-    }
+    // const [searchQuery, setSearchQuery] = useState("");
+    // const handleSearchQuery = function (searchQuery: string) {
+    //     setSearchQuery(searchQuery);
+    // }
     return (
         <header className="fixed top-0 flex justify-between items-center left-0 right-0 z-50 dark:bg-neutral-900 border dark:border-gray-500/20 backdrop-blur-lg py-1 px-2  rounded-b-xl shadow-lg dark:shadow-black">
             <NewHiIcon className='w-[2rem]' animate={true}/>
